a9c4e29ae7a563fe187fb08ed730ba51c2043c7f
Unknown <mccurdyskyler@gmail.com>
2017-01-10 18:29:26 -0700

    Added color coding to console target

--- LogginIt/Target/ConsoleTarget.cs
+++ LogginIt/Target/ConsoleTarget.cs
@@ -10,10 +10,22 @@ namespace LogginIt.Target
     {
         private static object s_lock = new object();
 
+        private static Dictionary<LogLevel, ConsoleColor> colors;
+        static ConsoleTarget()
+        {
+            colors = new Dictionary<LogLevel, ConsoleColor>();
+            colors[LogLevel.DEBUG] = ConsoleColor.Blue;
+            colors[LogLevel.ERROR] = ConsoleColor.Red;
+            colors[LogLevel.INFO] = ConsoleColor.White;
+            colors[LogLevel.WARNING] = ConsoleColor.Yellow;
+        }
+
         public void log(LogLevel level, string source, string message, Exception exception)
         {
             lock (s_lock)
-            {
+            {   
+                Console.ForegroundColor = colors[level];
+
                 Console.WriteLine(level.ToString() + " " + source + ": " + message);
                 if (exception != null)
                 {
